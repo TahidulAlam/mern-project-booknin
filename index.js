674af46c45faae23340de8ccf33f510611506b10
TahidulAlam <tahid.cse@gmail.com>
2023-11-08 13:46:39 +0600

    In this git i add update books, quantity increase and decrease

--- index.js
+++ index.js
@@ -42,6 +42,7 @@ const bookninDB = client.db("bookninDB");
 const booksCollections = bookninDB.collection("booksCollections");
 const categoryCollection = bookninDB.collection("categoryCollection");
 const ImageCollection = bookninDB.collection("ImageCollection");
+const borrowedCollection = bookninDB.collection("borrowedCollection");
 
 app.get("/api/bn/image", async (req, res) => {
   try {
@@ -72,6 +73,122 @@ app.get("/api/bn/allbooks", async (req, res) => {
   } catch (error) {
     console.log(error);
     res.status(500).send("Internal Server Error");
+  }
+});
+app.get("/api/bn/allbooks/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const query = { _id: new ObjectId(id) };
+    const result = await booksCollections.findOne(query);
+    res.send(result);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+app.put("/api/bn/allbooks/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const UpdateContent = req.body;
+    const filter = { _id: new ObjectId(id) };
+    const option = { upsert: true };
+    const updatedProduct = {
+      $set: {
+        book_name: UpdateContent.book_name,
+        image_link: UpdateContent.image_link,
+        ratings: UpdateContent.ratings,
+        quantity: UpdateContent.quantity,
+        category: UpdateContent.category,
+        shortDescription: UpdateContent.shortDescription,
+        author_name: UpdateContent.author_name,
+      },
+    };
+    const result = await booksCollections.updateOne(
+      filter,
+      updatedProduct,
+      option
+    );
+    if (result.modifiedCount > 0) {
+      res.send(result);
+    } else {
+      res.status(404).json({ message: "Product not found" });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+app.post("/api/bn/borrowedBooks", async (req, res) => {
+  try {
+    const data = req.body;
+    const bookName = data.book_name;
+    const book = await booksCollections.findOne({ book_name: bookName });
+
+    if (!book) {
+      return res.status(404).json({ error: "Book not found" });
+    }
+
+    if (book.quantity <= 0) {
+      return res.status(400).json({ error: "Book is out of stock" });
+    }
+    await booksCollections.updateOne(
+      { _id: book._id },
+      { $inc: { quantity: -1 } }
+    );
+    const result = await borrowedCollection.insertOne(data);
+
+    res.json(result);
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .json({ error: "An error occurred while borrowing the book" });
+  }
+});
+
+app.get("/api/bn/borrowedBooks", async (req, res) => {
+  try {
+    // if (req.query.email !== req.user.email) {
+    //   return res.status(403).send({ message: "UnAutherised" });
+    // }
+    let query = {};
+    if (req.query?.email) {
+      query = { email: req.query.email };
+    }
+    const result = await borrowedCollection.find(query).toArray();
+    res.send(result);
+  } catch (error) {
+    console.log(error);
+  }
+});
+app.delete("/api/bn/borrowedBooks/:bookId", async (req, res) => {
+  try {
+    const id = req.params.bookId;
+    const data = req.body;
+    const bookName = data.book_name;
+    const query = { _id: new ObjectId(id) };
+    const result = await borrowedCollection.deleteOne(query);
+
+    if (result.deletedCount === 1) {
+      const book = await booksCollections.findOne({ book_name: bookName });
+      if (!book) {
+        return res.status(404).json({ error: "Book not found" });
+      }
+      if (book.quantity <= 0) {
+        return res.status(400).json({ error: "Book is out of stock" });
+      }
+      await booksCollections.updateOne(
+        { _id: book._id },
+        { $inc: { quantity: 1 } }
+      );
+
+      res.send({ success: true });
+    } else {
+      res.status(404).send({ error: "Document not found" });
+    }
+  } catch (error) {
+    console.error("Error deleting document:", error);
+    res.status(500).send({ error: "Internal server error" });
   }
 });
 
